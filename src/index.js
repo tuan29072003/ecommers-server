5913cbdd720a7bdc47e68a92fb4f48c6d009ce5d
tuan29072003 <trankhacanhtuan29072003@gmail.com>
2024-12-28 21:48:58 +0700

    server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 
-const express=require("express")
-const dotenv=require('dotenv');
+const express = require("express")
+const dotenv = require('dotenv');
 const { default: mongoose } = require("mongoose");
 const cors = require('cors');
 const UserRouter = require('./routers/UserRouter')
@@ -10,25 +10,23 @@ const ProductRouter = require('./routers/ProductRouter')
 
 dotenv.config();
 
-const app=express();
-const port=process.env.PORT||3001;
+const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(express.json())//chuyển dữ liệu sang kiểu json
 app.use(cors());
-app.use('/auth',UserRouter)
-app.use('/supplier',SupplierRouter)
-app.use('/products',ProductRouter)
+app.use('/auth', UserRouter)
+app.use('/supplier', SupplierRouter)
+app.use('/products', ProductRouter)
 
 
 
 
 mongoose.connect(`${process.env.MONGO_DB}`)
-.then(()=>
-    {console.log("connect db successfully")})
-.catch((err)=>
-    {console.log(err)})
+    .then(() => { console.log("connect db successfully") })
+    .catch((err) => { console.log(err) })
 
 
-app.listen(port,()=>{
-    console.log("server is running in port:",+port)
+app.listen(port, () => {
+    console.log("server is running in port:", +port)
 })
